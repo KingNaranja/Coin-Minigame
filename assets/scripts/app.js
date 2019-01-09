cf0ef09cf144f3e2ec8bc8b1fca0e23203feb3b8
KingNaranja <kledaa3@gmail.com>
2019-01-08 23:13:21 -0500

    loaded player and physics

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,46 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+// import 'phaser'
+// const scene = require('./game')
 
 $(() => {
-  // your JS code goes here
+  
+class mainScene {
+  // load game assets
+  preload() {
+    console.log('this loaded')
+    this.load.image('player','./assets/monster/player.png')
+  }
+  // initialize the scene 
+  create() {
+    // the player is stored inside this.player 
+    // create player with phsyics engine 
+    this.player = this.physics.add.sprite(100,180,'player')
+    
+  }
+  // game logic
+  update() {
+
+
+  }
+
+}
+
+const config = {
+  type: Phaser.auto,
+  width: 700,
+  height: 400,
+  backgroundColor: '#3498db',
+  parent: 'game',
+  scene:mainScene,
+  physics: {default: 'arcade'}
+  
+
+}
+
+
+const game = new Phaser.Game(config)
+
+
 })
