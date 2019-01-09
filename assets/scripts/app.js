cedac8a6bc85408b9568b6fbd4a64b0b2d2903aa
KingNaranja <kledaa3@gmail.com>
2019-01-09 01:40:16 -0500

    added logic to handle coin collision

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,7 +6,7 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 // import 'phaser'
-// const scene = require('./game')
+const scene = require('./game')
 
 $(() => {
   
@@ -23,20 +23,57 @@ class mainScene {
     // create player with phsyics engine 
     this.player = this.physics.add.sprite(100,180,'player')
     this.coin = this.physics.add.sprite(300,300, 'coin')
+
     // the game score is stored in a variable
     this.score = 0 
     let style = { font: '20px Arial', fill: '#fff' };
     // use style to display score in the top left corner
     this.scoreText = this.add.text(20, 20, 'score: ' + this.score, style);
 
-    
+    // define arrow key inputs 
+    this.arrow = this.input.keyboard.createCursorKeys();
   }
   // game logic
   update() {
+    // check if player is overlapping
+    // If the player is overlapping with the coin
+    if (this.physics.overlap(this.player, this.coin)) {
+      // Call the new hit() method
+      this.hit();
+    }   
 
+    // Handle horizontal movements
+    if (this.arrow.right.isDown) {
+      // If the right arrow is pressed, move to the right
+          this.player.x += 3;
+    } else if (this.arrow.left.isDown) {
+      // If the left arrow is pressed, move to the left
+          this.player.x -= 3;
+    } 
+    
+    // Do the same for vertical movements
+    if (this.arrow.down.isDown) {
+      this.player.y += 3;
+    } else if (this.arrow.up.isDown) {
+      this.player.y -= 3;
+    } 
+    
 
   }
 
+  // every time the player grabs a coin
+  // it is moved to a new position
+  hit() {
+    // randomly change the position of x and y
+    this.coin.x = Phaser.Math.Between(100,600)
+    this.coin.y = Phaser.Math.Between(100,300);
+
+    // increment the game score
+    this.score += 10
+
+    // display the updated score on the screen
+    this.scoreText.setText('score: ' + this.score)
+  }
 }
 
 const config = {
