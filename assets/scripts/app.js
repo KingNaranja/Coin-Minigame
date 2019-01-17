7a3b87ae451b9ac78f7d65bb38650b9c2a65aa6e
KingNaranja <kledaa3@gmail.com>
2019-01-17 10:56:17 -0500

    Implement user auth event listeners
    The auth directory will contain the logic for the user authorized routes
    to sign-up, sign-in, and sign-out.
    
    The addEvents function contains the jQuery event listeners for user
    forms.
    
    - The change-password and logout user actions will be accessed by a
      user-options modal after sign-in success.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,28 +1,29 @@
 'use strict'
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-// require('./example')
-// import 'phaser'
+const authEvents = require('./auth/events')
 import {mainScene} from './scenes/gameplay'
+
 
 $(() => {
   
-
-// initialize phaser game 
-const config = {
-  type: Phaser.auto,
-  width: 700,
-  height: 400,
-  backgroundColor: '#3498db',
-  parent: 'game',
-  scene:mainScene,
-  physics: {default: 'arcade'}
   
-}
-const game = new Phaser.Game(config)
 
+  // initialize phaser game 
+  const config = {
+    type: Phaser.auto,
+    width: 700,
+    height: 400,
+    backgroundColor: '#3498db',
+    parent: 'game',
+    scene:mainScene,
+    physics: {default: 'arcade'}
+    
+  }
+  const game = new Phaser.Game(config)
+
+  // adds user auth event handlers 
+  authEvents.addEvents()
+  
+  
 
 })
